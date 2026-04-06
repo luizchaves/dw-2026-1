e167d7d2ec9e7ecaf807da5577cddc7fdc12d2e3
Luiz Carlos <lucachaves@gmail.com>
2026-04-06 07:50:30 -0300

    classroom: Add initial implementation of math library with basic operations and configuration files

---
+++ classroom/ecma/modules/esm-npm/main.js
@@ -0,0 +1,16 @@
+// EcmaScript module (ESM) - Named and default exports
+import MathLib, { sum, subtract, multiply, divide } from './lib.js';
+import { sqrt } from 'mathjs'
+
+console.log(sum(10, 5)); // 15
+console.log(subtract(10, 5)); // 5
+console.log(multiply(10, 5)); // 50
+console.log(divide(10, 5)); // 2
+console.log(MathLib.sum(10, 5)); // 15
+console.log(MathLib.subtract(10, 5)); // 5
+console.log(MathLib.multiply(10, 5)); // 50
+console.log(MathLib.divide(10, 5)); // 2
+
+console.log(Math.sqrt(4)) // 2
+console.log(sqrt(4)) // 2
+console.log(sqrt(-4).toString()) // 2i
