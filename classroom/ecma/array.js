d497fe7c5462f8752f5656f196e472544ea60e11
Luiz Carlos <lucachaves@gmail.com>
2026-04-09 11:09:42 -0300

    classroom: Add examples for accessing array values using at() method

--- classroom/ecma/array.js
+++ classroom/ecma/array.js
@@ -5,6 +5,9 @@ const numbers = [1, 2, 3, 4, 5];
 console.log(numbers[0]); // 1
 console.log(numbers[2]); // 3
 console.log(numbers[4]); // 5
+console.log(numbers.at(-1)); // 5
+console.log(numbers.at(-2)); // 4
+console.log(numbers[5]); // undefined
 
 // adding values
 numbers.push(6); // adds 6 to the end of the array
