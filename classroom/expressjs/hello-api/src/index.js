2f9d9a4f94dea2e9aca2705ac90713225c4f0336
Luiz Carlos <lucachaves@gmail.com>
2026-05-11 08:41:33 -0300

    classroom: route params

--- classroom/expressjs/hello-api/src/index.js
+++ classroom/expressjs/hello-api/src/index.js
@@ -3,10 +3,38 @@ import morgan from 'morgan';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
+  res.send('Hello API');
+});
+
+app.get('/hello/en', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.get('/hello/pt', (req, res) => {
+  res.send('Olá, Mundo!');
+});
+
+app.get('/en/:name', (req, res) => {
+  const { name } = req.params;
+
+  res.json({ message: `Hello, ${name}!` });
+});
+
+app.get('/pt', (req, res) => {
+  const { name } = req.query;
+
+  res.json({ message: `Olá, ${name}!` });
+});
+
+app.post('/es', (req, res) => {
+  const { name } = req.body;
+
+  res.json({ message: `¡Hola, ${name}!` });
 });
 
 app.listen(3000, () => {
